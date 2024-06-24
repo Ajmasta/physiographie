90835969bf0630809f4733eba8ecfae27796b394
ajmasta <adrien.moevus@mail.mcgill.ca>
2024-06-24 08:39:06 -0400

    added logo and title

--- src/components/heroSection.tsx
+++ src/components/heroSection.tsx
@@ -1,30 +1,35 @@
 import { FaGraduationCap, FaBook } from "react-icons/fa6";
 import Text from "../i18n/fr/homepage.json";
 import HeroComponent from "./HeroComponent";
+import { Link } from "react-router-dom";
 export const HeroSection = () => {
   return (
     <div className="flex flex-wrap justify-center bg-white">
-      <HeroComponent
-        title={Text.cardSection.cardOne.title}
-        description={Text.cardSection.cardOne.description}
-      >
-        <div className="p-1 border-2 rounded-full avatar">
-          <div className="rounded-full bg-physioBlue ">
-            <FaGraduationCap size={60} className="m-6" color={"white"} />
+      <Link to="/theorie">
+        <HeroComponent
+          title={Text.cardSection.cardOne.title}
+          description={Text.cardSection.cardOne.description}
+        >
+          <div className="p-1 border-2 rounded-full avatar">
+            <div className="rounded-full bg-physioBlue ">
+              <FaGraduationCap size={60} className="m-6" color={"white"} />
+            </div>
           </div>
-        </div>
-      </HeroComponent>
+        </HeroComponent>
+      </Link>
 
-      <HeroComponent
-        title={Text.cardSection.cardTwo.title}
-        description={Text.cardSection.cardTwo.description}
-      >
-        <div className="p-1 border-2 rounded-full avatar">
-          <div className="rounded-full bg-physioBlue ">
-            <FaBook size={60} className="p-2 m-6" color={"white"} />
+      <Link to="/protocoles">
+        <HeroComponent
+          title={Text.cardSection.cardTwo.title}
+          description={Text.cardSection.cardTwo.description}
+        >
+          <div className="p-1 border-2 rounded-full avatar">
+            <div className="rounded-full bg-physioBlue ">
+              <FaBook size={60} className="p-2 m-6" color={"white"} />
+            </div>
           </div>
-        </div>
-      </HeroComponent>
+        </HeroComponent>
+      </Link>
     </div>
   );
 };
